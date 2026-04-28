df244b39a69f95488a394411380677fcc7ef6409
yen1 <yen1k1m124@naver.com>
2026-04-28 12:24:05 +0900

    Dto모음
    user, furniture, options, coupon, wallet, question, like, cart, furnitureReview, cartOption의 DTO입니다.

---
+++ src/main/java/com/spring/home/dto/FurnitureReviewDTO.java
@@ -0,0 +1,16 @@
+package com.spring.home.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class FurnitureReviewDTO {
+
+	private String id;
+	private String f_code;
+	private String fr_subject;
+	private String fr_star;
+	private Date fr_createdDate;
+	private String fr_content;
+}
---
+++ src/main/java/com/spring/home/dto/OptionsDTO.java
@@ -0,0 +1,14 @@
+package com.spring.home.dto;
+
+import lombok.Data;
+
+@Data
+public class OptionsDTO {
+
+	private String f_code;
+	private String o_select;
+	private String o_text;
+	private int o_count;
+	private int o_price;
+	private String o_important;
+}
