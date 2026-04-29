8d49e78c86185e8c16d72f1cf932233b1b6ede09
yen1 <yen1k1m124@naver.com>
2026-04-29 11:15:00 +0900

    furnitureMapper.xml
    update delete insert getLists(전체) getReadData(1개) 를 추가한 mapper.xml입니다.
    
    수정된 사항
    furniture service, mapper의 오타 수정
    searchValue 첫글자s가 대문자 >소문자로 수정
    
    controller는 미완성으로 완성되는대로 다시 올리겠습니다.

--- src/main/java/com/spring/home/FurnitureController.java
+++ src/main/java/com/spring/home/FurnitureController.java
@@ -1,8 +1,43 @@
 package com.spring.home;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.home.dto.FurnitureDTO;
+import com.spring.home.service.FurnitureService;
+import com.spring.home.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/furniture")
+@RequiredArgsConstructor
+@RestController
+@CrossOrigin("http://localhost:3000")
 public class FurnitureController {
 
+	@Resource
+	private FurnitureService furnitureService;
+	@Resource
+	private UserService userService;
+	
+	@GetMapping("/list")
+	public List<FurnitureDTO> getLists(
+			@RequestParam(defaultValue="1") int pageNum,
+			@RequestParam(defaultValue = "f_name") String searchKey,
+			@RequestParam(defaultValue = "") String searchValue) throws Exception{
+		
+		int numPerPage = 5;
+		int start = (pageNum -1) * numPerPage + 1;
+		int end = pageNum * numPerPage;
+		
+		return furnitureService.getLists(start, end, searchKey, searchValue);
+		
+	}
 }
