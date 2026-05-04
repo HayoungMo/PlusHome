30f03791066c34ac0045ad08ae598edff143a473
yen1 <yen1k1m124@naver.com>
2026-05-04 16:26:47 +0900

    05.04 김예니
    furnitureadd 를 만들다가 많은 일이 있었던 코드 입니다. 살려주세요

--- src/main/java/com/spring/home/util/furnitureCode.java
+++ src/main/java/com/spring/home/util/furnitureCode.java
@@ -1,22 +1,11 @@
 package com.spring.home.util;
 
 import java.security.SecureRandom;
+import java.util.UUID;
 
 public class furnitureCode {
 
-	private static SecureRandom random = new SecureRandom();
-	
-	public static String generateCode(int companyCode, int catagoryCode) {
-		if(companyCode < 0 || companyCode > 9) {
-			throw new IllegalArgumentException("회사 코드는 0~9까지의 숫자를 사용해주세요");	
-		}
-		
-		if(catagoryCode < 0 || catagoryCode > 99) {
-			throw new IllegalArgumentException("카테고리 코드는 0~99까지의 숫자를 사용해주세요");	
-		}	
-		
-		int randomNum = random.nextInt(100000);
-		
-		return String.format("%d%02d%05d", companyCode, catagoryCode, randomNum);
+	public static String generateCode() {
+		return "F-" + UUID.randomUUID().toString();
 	}
 }
