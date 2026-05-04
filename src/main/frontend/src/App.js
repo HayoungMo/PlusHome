9fe1f51c4f0478db164163e39b7844e2035322c0
모하영 <mohayoung80@gmail.com>
2026-05-04 17:25:50 +0900

    0504 모하영
    챗봇 페이지, 챗봇 페이지 라우트 주소, 메인 화면 구성

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -21,6 +21,7 @@ import WalletCharge from "./pages/WalletCharge";
 import FurnitureList from "./pages/FurnitureList";
 import FurnitureAddPage from "./pages/FurnitureAddPage";
 import FurnitureArticle from "./pages/FurnitureArticle";
+import Chatbotpage from "./pages/Chatbotpage";
 
 function App() {
 	const navigate = useNavigate();
@@ -55,6 +56,7 @@ function App() {
         <Route path="/interior/question" element={<InteriorQuestion />} />
 		<Route path="/exportPDF" element={<ExportPDF />} />
 		<Route path="/exportPDFViewPage" element={<ExportPDFViewPage />} />
+    <Route path="/chatbot" element={<Chatbotpage />} />
       </Routes>
     </div>
   );
