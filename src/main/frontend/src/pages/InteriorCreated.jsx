1a65b3da09be0ba3c6283b2e34bcda01b5944454
dgty1212 <dgty1212@gmail.com>
2026-05-07 12:45:26 +0900

    0507유지엽

--- src/main/frontend/src/pages/InteriorCreated.jsx
+++ src/main/frontend/src/pages/InteriorCreated.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 import InteriorAdd from "../components/InteriorAdd";
 import InteriorExAdd from "../components/InteriorExAdd";
-import InteriorBookingLists from "../components/InteriorBookingLists";
 import InteriorChart from "../components/InteriorChart";
 import InteriorUpdate from "../components/InteriorUpdate";
 import InteriorExUpdate from "../components/InteriorExUpdate";
@@ -11,9 +10,9 @@ import BookingUpdate from "../components/BookingUpdate";
 //테스트용 파일
 function InteriorCreated(/*{ company }*/) {
   const [company, setCompany] = useState({
-    c_id: "comp02",
+    c_id: "test",
     c_kind: "interior",
-    c_name: "감성인테리어",
+    c_name: "인테리어",
   });
   
   return (
@@ -22,15 +21,6 @@ function InteriorCreated(/*{ company }*/) {
     <InteriorAdd company={company}/>
 
     <InteriorExAdd company={company}/>
-
-    <InteriorUpdate company={company}/>
-
-    <InteriorExUpdate company={company}/>
-
-    <BookingUpdate company={company}/>
-
-    <InteriorChart company={company}/>
-
      
     </div>
   );
