e48829ee26fb4ea14863e82688253d6337314a1f
Polalise <qudgus182@gmail.com>
2026-05-18 14:32:53 +0900

    0518 유병현

--- src/main/frontend/src/dasboardPages/ShoppingMallQuestionControl.jsx
+++ src/main/frontend/src/dasboardPages/ShoppingMallQuestionControl.jsx
@@ -1,13 +1,115 @@
-import React from 'react'
+import { Box, Tab, Tabs } from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
+import TableMui from "./../components/TableMui";
+import questionService from "../service/questionService";
+import AlertMui from "../components/AlertMui";
+import TextFieldMui from "./../components/TextFieldMui";
 
 const ShoppingMallQuestionControl = () => {
-  return (
-    <div>
-      ShoppingMallQuestionControl
+	const localUserData = localStorage.getItem("user");
+	const userData = JSON.parse(localUserData);
+	const { addr, birth, code, email, gender, id, name, tel, type, companyList = [] } = userData;
 
-      ShoppingMallQuestionControl
-    </div>
-  )
-}
+	const initAlertInfo = { severity: "", title: "", text: "" };
 
-export default ShoppingMallQuestionControl
+	const [tabValue, setTabValue] = useState("all");
+	const [selectedTabCompany, setSelectedTabCompany] = useState(null);
+	const [selectedRow, setSelectedRow] = useState(null);
+	const [allQuestionList, setAllQuestionList] = useState([]);
+	const [alertOpen, setAlertOpen] = useState(false);
+	const [alertInfo, setAlertInfo] = useState(initAlertInfo);
+
+	const shopListState = useMemo(() => {
+		const shopList = companyList.filter((data) => data.c_kind === "shop");
+
+		return [{ c_id: id, c_name: "all" }, ...shopList];
+	}, [companyList]);
+
+	const displayQuestionList = useMemo(() => {
+		if (tabValue === "all") return allQuestionList;
+		else return allQuestionList.filter((data) => data.c_name === tabValue);
+	}, [allQuestionList, tabValue]);
+
+	const handleTabChange = (event, newValue) => {
+		setTabValue(newValue);
+
+		const selectedCompany = shopListState.find((record) => record.c_name === newValue);
+		setSelectedTabCompany(selectedCompany);
+		setSelectedRow(null);
+	};
+
+	const reloadData = async () => {
+		const result = await questionService.getCompanyQuestions(id);
+		if (!result) {
+			setAlertInfo({
+				severity: "error",
+				title: "조회 실패",
+				text: "데이터 조회에 실패했습니다",
+			});
+		} else if (result.length === 0) {
+			setAlertInfo({
+				severity: "info",
+				title: "데이터 없음",
+				text: "등록된 문의가 없습니다.",
+			});
+		} else {
+			setAlertInfo({
+				severity: "success",
+				title: "조회 성공",
+				text: "성공적으로 데이터가 조회되었습니다",
+			});
+			setAllQuestionList(result);
+		}
+	};
+
+	useEffect(() => {
+		reloadData();
+	}, [id]);
+
+	return (
+		<div>
+			<h2>test</h2>
+			<Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
+				<Tabs value={tabValue} onChange={handleTabChange}>
+					{shopListState.map((record, index) => {
+						return (
+							<Tab
+								key={`${record.c_id}__${index}`}
+								label={record.c_name}
+								value={record.c_name}
+							/>
+						);
+					})}
+				</Tabs>
+			</Box>
+
+			<TableMui
+				col={["f_name", "c_name", "q_idx", "q_secret", "q_status", "id"]}
+				rowData={displayQuestionList}
+				selectedRow={selectedRow}
+				setSelectedRow={setSelectedRow}
+			/>
+			{selectedRow && (
+				<div>
+					<TextFieldMui label="상품명" value={selectedRow.f_name} />
+					<TextFieldMui label="문의 제목" value={selectedRow.q_title} />
+					<TextFieldMui label="문의 내용" value={selectedRow.q_content} />
+				</div>
+			)}
+
+			{alertOpen && (
+				<AlertMui
+					severity={alertInfo?.severity}
+					variant="standard"
+					title={alertInfo?.title}
+					text={alertInfo?.text}
+					onClose={() => setAlertOpen(!alertOpen)}
+					autoHideDuration={3000}
+					icon={true}
+				/>
+			)}
+		</div>
+	);
+};
+
+export default ShoppingMallQuestionControl;
--- src/main/frontend/src/service/interiorUserService.js
+++ src/main/frontend/src/service/interiorUserService.js
@@ -1,145 +1,155 @@
 import http from "../http-common";
 
 const fetchBookingList = async (id) => {
-  console.log("유저 예약 목록");
-  try {
-    const res = await http.post("interior/bookinglists", {
-      id: id,
-    });
+	console.log("유저 예약 목록");
+	try {
+		const res = await http.post("interior/bookinglists", {
+			id: id,
+		});
 
-    console.log("응답 데이터:", res.data);
-    return res.data;
-  } catch (err) {
-    console.error("에러:", err);
-  }
+		console.log("응답 데이터:", res.data);
+		return res.data;
+	} catch (err) {
+		console.error("에러:", err);
+	}
 };
 
 const fetchInvoice = async (data) => {
-  console.log("인보이스에 들어온 데이터", data);
-  try {
-    const res = await http.post("interior/invoice", {
-      id: data.id,
-      c_id: data.c_id,
-      c_kind: data.c_kind,
-      c_name: data.c_name,
-      b_createdDate: data.b_createdDate,
-    });
+	console.log("인보이스에 들어온 데이터", data);
+	try {
+		const res = await http.post("interior/invoice", {
+			id: data.id,
+			c_id: data.c_id,
+			c_kind: data.c_kind,
+			c_name: data.c_name,
+			b_createdDate: data.b_createdDate,
+		});
 
-    console.log("응답 데이터:", res.data);
-    return res.data;
-  } catch (err) {
-    console.error("에러:", err);
-  }
+		console.log("응답 데이터:", res.data);
+		return res.data;
+	} catch (err) {
+		console.error("에러:", err);
+	}
 };
 
 const fetchInvoiceDetails = async (data) => {
-  console.log("인보이스 디테일에 들어온 데이터", data);
-  try {
-    const res = await http.post("interior/invoicedetails", {
-      id: data.id,
-      c_id: data.c_id,
-      c_kind: data.c_kind,
-      c_name: data.c_name,
-      b_createdDate: data.b_createdDate,
-      invoice_no: data.invoice_no,
-      invoice_qty: data.invoice_qty,
-      invoice_kind: data.invoice_kind,
-    });
+	console.log("인보이스 디테일에 들어온 데이터", data);
+	try {
+		const res = await http.post("interior/invoicedetails", {
+			id: data.id,
+			c_id: data.c_id,
+			c_kind: data.c_kind,
+			c_name: data.c_name,
+			b_createdDate: data.b_createdDate,
+			invoice_no: data.invoice_no,
+			invoice_qty: data.invoice_qty,
+			invoice_kind: data.invoice_kind,
+		});
 
-    console.log("응답 데이터:", res.data);
-    return res.data;
-  } catch (err) {
-    console.error("에러:", err);
-  }
+		console.log("응답 데이터:", res.data);
+		return res.data;
+	} catch (err) {
+		console.error("에러:", err);
+	}
 };
 
 const fetchInteriorReview = async (id) => {
-  try {
-    const res = await http.post("interior/userreview", {
-      id: id,
-      invoice_kind: "Y",
-    });
+	try {
+		const res = await http.post("interior/userreview", {
+			id: id,
+			invoice_kind: "Y",
+		});
 
-    console.log("응답 데이터:", res.data);
-    return res.data;
-  } catch (err) {
-    console.error("에러:", err);
-  }
+		console.log("응답 데이터:", res.data);
+		return res.data;
+	} catch (err) {
+		console.error("에러:", err);
+	}
 };
 
 const AddInteriorReview = async (data) => {
-  console.log("인테리어 리뷰 들어온 데이터", data);
-  try {
-    const res = await http.post("/interior/add/review", {
-      id: localStorage.getItem("id"),
-      c_id: data.c_id,
-      c_kind: data.c_kind,
-      c_name: data.c_name,
-      b_createdDate: data.b_createdDate,
-      invoice_no: data.invoice_no,
-      invoice_kind: data.invoice_kind,
-      ir_content: data.ir_content,
-    });
+	console.log("인테리어 리뷰 들어온 데이터", data);
+	try {
+		const res = await http.post("/interior/add/review", {
+			id: localStorage.getItem("id"),
+			c_id: data.c_id,
+			c_kind: data.c_kind,
+			c_name: data.c_name,
+			b_createdDate: data.b_createdDate,
+			invoice_no: data.invoice_no,
+			invoice_kind: data.invoice_kind,
+			ir_content: data.ir_content,
+		});
 
-    return {
-      success: true,
-      data: res.data,
-    };
-  } catch (err) {
-    console.error(err);
-  }
+		return {
+			success: true,
+			data: res.data,
+		};
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 const UpdateInteriorReview = async (data) => {
-  try {
-    const res = await http.post("/interior/update/interiorreview", {
-      id: data.id,
-      c_id: data.c_id,
-      c_kind: data.c_kind,
-      c_name: data.c_name,
-      b_createdDate: data.b_createdDate,
-      invoice_no: data.invoice_no,
-      invoice_kind: data.invoice_kind,
-      ir_content: data.ir_content,
-    });
+	try {
+		const res = await http.post("/interior/update/interiorreview", {
+			id: data.id,
+			c_id: data.c_id,
+			c_kind: data.c_kind,
+			c_name: data.c_name,
+			b_createdDate: data.b_createdDate,
+			invoice_no: data.invoice_no,
+			invoice_kind: data.invoice_kind,
+			ir_content: data.ir_content,
+		});
 
-    console.log("예약 수정 결과:", res.data);
-    return {
-      success: true,
-      data: res.data,
-    };
-  } catch (err) {
-    console.error("예약 수정 에러:", err);
-        return {
-      success: false
-    }
-  }
+		console.log("예약 수정 결과:", res.data);
+		return {
+			success: true,
+			data: res.data,
+		};
+	} catch (err) {
+		console.error("예약 수정 에러:", err);
+		return {
+			success: false,
+		};
+	}
 };
 
 const DeleteInteriorReview = async (data) => {
-  try {
-    const res = await http.post("/interior/delete/interiorreview", {
-      id: data.id,
-      c_id: data.c_id,
-      c_kind: data.c_kind,
-      c_name: data.c_name,
-      b_createdDate: data.b_createdDate,
-      invoice_no: data.invoice_no,
-      invoice_kind: data.invoice_kind,
-    });
-  } catch (err) {
-    console.error("삭제 에러:", err);
-  }
+	try {
+		const res = await http.post("/interior/delete/interiorreview", {
+			id: data.id,
+			c_id: data.c_id,
+			c_kind: data.c_kind,
+			c_name: data.c_name,
+			b_createdDate: data.b_createdDate,
+			invoice_no: data.invoice_no,
+			invoice_kind: data.invoice_kind,
+		});
+	} catch (err) {
+		console.error("삭제 에러:", err);
+	}
+};
+
+const getInteriorReviewByCompanyId = async (data) => {
+	try {
+		const res = await http.post("/interior/select/getInteriorReviewByCompanyId", data);
+		return res.data;
+	} catch (err) {
+		console.error("getInteriorReviewByCompanyId 에러:", err);
+	}
 };
 
 const InteriorUserService = {
-  fetchBookingList,
-  fetchInvoice,
-  fetchInvoiceDetails,
-  fetchInteriorReview,
-  AddInteriorReview,
-  UpdateInteriorReview,
-  DeleteInteriorReview,
+	fetchBookingList,
+	fetchInvoice,
+	fetchInvoiceDetails,
+	fetchInteriorReview,
+	AddInteriorReview,
+	UpdateInteriorReview,
+	DeleteInteriorReview,
+	getInteriorReviewByCompanyId,
 };
 
 export default InteriorUserService;
